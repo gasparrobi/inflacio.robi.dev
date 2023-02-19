b82a649cb146ef5e273acaab0ea4d9813cd8b04c
gasparrobi <gasparrobi@gmail.com>
2023-02-19 19:52:15 +0100

    init

---
+++ src/components/chart/BarChart.tsx
@@ -0,0 +1,163 @@
+import { ResponsiveBar } from "@nivo/bar";
+import { inflation } from "../../inflation";
+import { formatCurrency } from "../../utils/formatCurrency";
+import { useWindowSize } from "react-use";
+
+export interface InflationItem {
+  year: number;
+  january: number;
+  february?: number;
+  march?: number;
+  april?: number;
+  may?: number;
+  june?: number;
+  july?: number;
+  august?: number;
+  september?: number;
+  october?: number;
+  november?: number;
+  december?: number;
+}
+
+interface BarChartProps {
+  payload?: InflationItem[];
+  isAmountHidden?: boolean;
+  selectedYear?: number;
+}
+
+const BarChart = ({
+  payload,
+  isAmountHidden = false,
+  selectedYear = 2023,
+}: BarChartProps) => {
+  const _payload = payload ?? inflation;
+
+  const { width } = useWindowSize();
+  const isMobileScreen = width < 768;
+
+  return (
+    <ResponsiveBar
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      data={_payload}
+      keys={["january"]}
+      indexBy="year"
+      margin={{ top: 50, right: 30, bottom: 50, left: 80 }}
+      padding={0.3}
+      groupMode="grouped"
+      enableLabel={false}
+      // enableGridY={false}
+      valueScale={{ type: "linear" }}
+      indexScale={{ type: "band", round: true }}
+      // colors={{ scheme: "dark2" }}
+      // colors={"#1b9e77"}
+      colors={(element) =>
+        element.data.year === selectedYear ? "#fff" : "#1b9e77"
+      }
+      colorBy="id"
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]],
+      }}
+      theme={{
+        axis: {
+          ticks: {
+            line: {
+              stroke: "white",
+            },
+            text: {
+              fill: "white",
+            },
+          },
+        },
+        grid: {
+          line: {
+            stroke: "rgba(255,255,255,.3)",
+          },
+        },
+        legends: {
+          text: {
+            fill: "white",
+          },
+        },
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisLeft={
+        isAmountHidden
+          ? null
+          : {
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legendPosition: "middle",
+              legendOffset: -55,
+              format: (value) => formatCurrency(value as number),
+            }
+      }
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: isMobileScreen ? -60 : 0,
+        legend: "",
+        legendPosition: "middle",
+        legendOffset: 32,
+      }}
+      // axisLeft={{
+      //   tickSize: 5,
+      //   tickPadding: 5,
+      //   tickRotation: 0,
+      //   legend: 'összeg',
+      //   legendPosition: 'middle',
+      //   legendOffset: -55,
+      // }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]],
+      }}
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: "top",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: -40,
+          itemsSpacing: 2,
+          itemWidth: 100,
+          itemHeight: 20,
+          itemDirection: "left-to-right",
+          itemOpacity: 0.85,
+          symbolSize: 20,
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+      tooltip={(e) => {
+        return (
+          <div
+            style={{
+              background: "white",
+              padding: "9px 12px",
+              border: "1px solid #ccc",
+            }}
+          >
+            <div className="font-bold">{e.data.year} - január</div>
+            <div>{formatCurrency(e.data.january as number)}</div>
+          </div>
+        );
+      }}
+      role="application"
+    />
+  );
+};
+
+export default BarChart;
---
+++ src/pages/index.tsx
@@ -0,0 +1,159 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+import BarChart, { type InflationItem } from "../components/chart/BarChart";
+import DropdownSelector from "../components/ui/DropdownSelector";
+import { inflation } from "../inflation";
+import { useState } from "react";
+import { TextInput } from "../components/ui/TextInput";
+import Link from "next/link";
+import { Highlighted } from "../components/ui/Highlighted";
+import { formatCurrency } from "../utils/formatCurrency";
+
+const years = inflation.map((item) => item.year).sort((a, b) => b - a);
+
+const Home: NextPage = () => {
+  const [selectedYear, setSelectedYear] = useState(2018);
+  const [selectedAmount, setSelectedAmount] = useState(100_000);
+
+  const { january: selectedInflationValue } = inflation.find(
+    (item) => item.year === selectedYear
+  ) as InflationItem;
+  const baseValue = (selectedAmount / selectedInflationValue) * 100;
+
+  const _inflation = inflation.map((item) => {
+    const { year, january } = item;
+    return { year, january: ((baseValue * january) / 100).toFixed() };
+  }) as unknown as InflationItem[];
+
+  const _valueDegradation = inflation.map((item) => {
+    const { year, january } = item;
+
+    return {
+      year,
+      january: (selectedAmount * (selectedInflationValue / january)).toFixed(),
+    };
+  }) as unknown as InflationItem[];
+
+  const valueToday = _inflation.find((item) => item.year === 2023)
+    ?.january as number;
+
+  const degradedValueToday = _valueDegradation.find(
+    (item) => item.year === 2023
+  )?.january as number;
+
+  const handleYearSelection = (value: number | string) => {
+    setSelectedYear(Number(value));
+  };
+
+  const onAmountChange = (event: React.SyntheticEvent) => {
+    const value = (event.target as HTMLInputElement).value;
+    if (Number(value) > 0) setSelectedAmount(Number(value));
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Infláció kalkulátor</title>
+        <meta
+          name="description"
+          content="Számold ki mennyit romlott a forint értéke az évek során"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          defer
+          data-website-id={process.env.STATS_WEBSITE_ID}
+          src={process.env.STATS_URL}
+        ></script>
+      </Head>
+      {/* <main className="flex min-h-screen flex-col items-center  bg-gradient-to-b from-[#2e026d] to-[#15162c]"> */}
+      <main className="relative flex h-full min-h-screen flex-col items-center  bg-[#17161e]">
+        <div className="absolute inset-0 z-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(to_bottom,transparent,black)]"></div>
+        <div className="container z-10 flex flex-col items-center justify-center px-4 py-16 ">
+          <div className=" flex min-h-[400px]  w-full  flex-col gap-20 ">
+            <div>
+              <h1 className="text-4xl font-extrabold text-white sm:text-6xl">
+                Infláció kalkulátor
+              </h1>
+              <p className="text-lg text-slate-400">
+                Érdekel, hogy mennyit ér vagy éppen mennyit romlott a pénzed
+                értéke az évek során?
+              </p>
+            </div>
+
+            <div className="flex flex-wrap items-center gap-4">
+              <span className="font-bold text-white">Számold ki, hogy</span>
+              <TextInput
+                name="amount"
+                defaultValue={100_000}
+                type="number"
+                onBlur={onAmountChange}
+              />
+              <span className="font-bold text-white">Forint</span>
+              <DropdownSelector
+                options={years}
+                defaultValue={selectedYear}
+                onChange={handleYearSelection}
+              />{" "}
+              <span className="font-bold text-white">
+                -ban/ben mennyit ér ma
+              </span>
+            </div>
+
+            <div className="flex flex-col gap-4  py-3 text-lg font-bold text-white">
+              <p>
+                Ez azt jelenti, hogy ami{" "}
+                <Highlighted> {selectedYear} </Highlighted> évben{" "}
+                <Highlighted> {formatCurrency(selectedAmount)}</Highlighted> -ba
+                került, azt ma{" "}
+                <Highlighted> {formatCurrency(valueToday)}</Highlighted> -ért
+                tudod megvenni 🤌
+              </p>
+            </div>
+
+            <div className="h-[400px] min-h-[400px]">
+              <BarChart payload={_inflation} selectedYear={selectedYear} />
+            </div>
+
+            <div className="flex flex-col gap-4  py-3 text-lg font-bold text-white">
+              <p>
+                Ez azt is jelentheti, hogy a{" "}
+                <Highlighted> {selectedYear} </Highlighted> évben kapott{" "}
+                <Highlighted> {formatCurrency(selectedAmount)}</Highlighted>
+                fizetésednek az értéke ma{" "}
+                <Highlighted>
+                  {" "}
+                  {formatCurrency(degradedValueToday)}
+                </Highlighted>{" "}
+                🤌
+              </p>
+            </div>
+
+            <div className="h-[400px] min-h-[400px]">
+              <BarChart
+                payload={_valueDegradation}
+                selectedYear={selectedYear}
+              />
+            </div>
+
+            <div className="w-full text-center text-sm text-white">
+              A felhasznált értékek a{" "}
+              <Link
+                className="text-primary underline"
+                target="_blank"
+                rel="noopener noreferrer"
+                href={"https://www.ksh.hu/stadat_files/ara/hu/ara0045.html"}
+              >
+                ksh magáninflációs adatbázisából{" "}
+              </Link>
+              származnak.
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
